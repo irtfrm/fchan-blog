e2777295fec6c98b42f44804c3c3cd3f369ab51e
irtfrm <frm.tomoya@gmail.com>
2021-02-10 01:10:06 +0900

    componentからcssを分離

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,9 @@
+import styles from './footer.module.css'
+
 export default function Footer() {
   return (
-    <>
-      <footer>
-        © {new Date().getFullYear()} Tomoya Furukawa
+      <footer className={styles.footer}>
+        ©{new Date().getFullYear()} Tomoya Furukawa
       </footer>
-      <style jsx>{`
-        footer {
-          width: 100%;
-          height: 86px;
-          font-size: 86%;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-    </>
   )
 }
--- components/PostDetail.js
+++ components/PostDetail.js
@@ -1,17 +1,18 @@
 import Link from 'next/link'
 import Folder from './icons/Folder'
 import { PostContent } from './PostContent';
+import styles from './post.module.css'
 
 function PostDetail({ pid, date, category, title, content }) {
   return (
-    <div className="post">
-      <div className="text">
-      <Link href={"/posts/"+pid}><h2>{title}</h2></Link>
-      <div className="info">
+    <div className={styles.post}>
+      <div className={styles.text}>
+      <Link href={"/posts/"+pid}><h2 className={styles.title}>{title}</h2></Link>
+      <div className={styles.info}>
           <span>
             {date.substring(0, 10)}
           </span>
-          <span className="center-dot">&middot;</span>
+          <span className={styles.center_dot}>&middot;</span>
           <span> 
             <Folder/>
             {category}
@@ -19,49 +20,6 @@ function PostDetail({ pid, date, category, title, content }) {
         </div>
         <PostContent content={content}/>
       </div>
-
-      <style jsx>{`
-        .post {
-          position: relative;
-          margin: 10px;
-          width: 600px;
-          border-radius: 2px;
-          transition: .2s;
-        }
-        .text {
-          padding: 10px 0 0;
-          box-sizing: border-box;
-          width: 100%;
-        }
-        h2,
-        .info,
-        p {
-          margin: 8px 5px;
-        }
-        h2 {
-          margin-bottom: 0;
-          cursor: pointer;
-          line-height: 1.6;
-        }
-        h2:hover {
-          text-decoration: underline;
-        }
-        .info {
-          margin-top: 0;
-          margin-bottom: 16px;
-          font-size: 0.8em;
-          color: #9aafb4;
-        }
-        .center-dot {
-          text-align: center;
-          margin: 0 1em;
-        }
-        @media screen and (max-width: 700px){
-          .post {
-            width: 95vw;
-          }
-        }
-      `}</style>
     </div>
   )
 }
