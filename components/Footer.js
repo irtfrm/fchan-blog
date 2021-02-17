e24a51fa2ebbf28957549df0bacbcc8dc57ce67f
irtfrm <frm.tomoya@gmail.com>
2021-02-18 04:15:41 +0900

    コンタクトしろ感が強すぎたからfooterにアイコン群を移した

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,19 @@
 import styles from './footer.module.css'
+import GitHub from './icons/GitHub'
+import LinkedIn from './icons/LinkedIn'
+import Mail from './icons/Mail'
 
 export default function Footer() {
   return (
       <footer className={styles.footer}>
+        <div className={styles.icon_container}>
+          <a target="_blank" className={styles.icon} href="mailto:frm.tomoya@gmail.com"><Mail/> </a>
+          <a target="_blank" className={styles.icon} href="https://github.com/irtfrm"><GitHub/> </a>
+          <a target="_blank" className={styles.icon} href="https://www.linkedin.com/in/tomoya-furukawa-0ba37a203/"><LinkedIn/> </a>
+        </div>
+        <div>
         ©{new Date().getFullYear()} Tomoya Furukawa
+        </div>
       </footer>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,10 @@
 import Link from 'next/link'
 import styles from './header.module.css'
-import GitHub from './icons/GitHub'
-import LinkedIn from './icons/LinkedIn'
-import Mail from './icons/Mail'
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <Link href="/"><h1 className={styles.title}>fchan.dev</h1></Link>
-      <div className={styles.iconContainer}>
-        <a target="_blank" className={styles.icon} href="mailto:frm.tomoya@gmail.com"><Mail/> Mail</a>
-        <a target="_blank" className={styles.icon} href="https://github.com/irtfrm"><GitHub/> GitHub</a>
-        <a target="_blank" className={styles.icon} href="https://www.linkedin.com/in/tomoya-furukawa-0ba37a203/"><LinkedIn/> LinkedIn</a>
-      </div>
     </div>
   )
 }
