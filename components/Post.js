1d63f00c5135714a72a86b1f143aea3a48736be1
irtfrm <frm.tomoya@gmail.com>
2021-02-24 01:43:50 +0900

    読了時間実装、アイコン追加

--- components/Post.js
+++ components/Post.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Folder from './icons/Folder'
 import { PostDescription } from './PostDescription'
 import styles from './post.module.css'
+import {getReadingTimeFromMarkdown} from '@utils/post'
+import HourGlass from './icons/HourGlass';
 
 function Post({ pid, date, category, content, title }) {
   return (
@@ -11,12 +13,18 @@ function Post({ pid, date, category, content, title }) {
         <p className={styles.description}><PostDescription content={content}/></p>
         <div className={styles.info}>
           <span>
+            <Calender/>
             {date.substring(0, 10)}
           </span>
           <span className={styles.center_dot}>&middot;</span>
           <span> 
             <Folder/>
             {category}
+          </span>
+          <span className={styles.center_dot}>&middot;</span>
+          <span>
+            <HourGlass/>
+            {getReadingTimeFromMarkdown(content) + "分"}
           </span>
         </div>
       </div>
--- components/PostDetail.js
+++ components/PostDetail.js
@@ -2,6 +2,9 @@ import Link from 'next/link'
 import Folder from './icons/Folder'
 import { PostContent } from './PostContent';
 import styles from './post.module.css'
+import {getReadingTimeFromMarkdown} from '@utils/post'
+import HourGlass from './icons/HourGlass';
+import Calender from './icons/Calender';
 
 function PostDetail({ pid, date, category, title, content }) {
   return (
@@ -10,12 +13,18 @@ function PostDetail({ pid, date, category, title, content }) {
       <Link href={"/posts/"+pid}><h2 className={styles.title}>{title}</h2></Link>
       <div className={styles.info}>
           <span>
+            <Calender/>
             {date.substring(0, 10)}
           </span>
           <span className={styles.center_dot}>&middot;</span>
           <span> 
             <Folder/>
             {category}
+          </span>
+          <span className={styles.center_dot}>&middot;</span>
+          <span>
+            <HourGlass/>
+            {getReadingTimeFromMarkdown(content) + "分"}
           </span>
         </div>
         <PostContent content={content}/>
