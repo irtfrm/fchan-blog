25c7a05ad067fc0239a886339bfb5851226ea2ac
irtfrm <frm.tomoya@gmail.com>
2021-02-23 21:28:42 +0900

    開発中の component を import しようとしてたので comment out

--- pages/posts/[pid].js
+++ pages/posts/[pid].js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import ShareButtons from '@components/ShareButtons'
+//import ShareButtons from '@components/ShareButtons'
 import PostDetail from '@components/PostDetail'
 import { fetchPosts, fetchPostById } from '@utils/contentfulPosts'
 
