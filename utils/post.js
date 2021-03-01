5423bdf9ebfc1f16ca06f9c13a333cc3d882309e
irtfrm <frm.tomoya@gmail.com>
2021-03-01 22:30:06 +0900

    不要なコメント削除

--- utils/post.js
+++ utils/post.js
@@ -1,10 +1,8 @@
 export function countCharactersFromMarkdown(markdown){
-    console.log(markdown);
     let trimmed_markdown = markdown.replace(/\[?http.+?[\]\s]/g, "");
     trimmed_markdown = trimmed_markdown.replace(/\(\/\/images.+?\)/g, "");
     trimmed_markdown = trimmed_markdown.replace(/`{3}(.|\n|\r\n)+?`{3}/g, "");
     trimmed_markdown = trimmed_markdown.replace(/\s+/g, "");
-    console.log(trimmed_markdown);
     return trimmed_markdown.length;
 }
 
