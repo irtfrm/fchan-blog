822dd3f7db4e8a26cd47a59f291739fce47f191a
irtfrm <frm.tomoya@gmail.com>
2021-05-19 20:49:14 +0900

    minor ui changes

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,7 @@ export default function Footer() {
           <a target="_blank" className={styles.icon} href="https://www.linkedin.com/in/tomoya-furukawa-0ba37a203/"><LinkedIn/> </a>
         </div>
         <div className={styles.credit}>
-        ©{new Date().getFullYear()} Tomoya Furukawa
+        ©{new Date().getFullYear()} fchan
         </div>
       </footer>
   )
